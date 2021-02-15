4f329967e56bfc49aee063c99473f43c76ad8667
Raphaëlle Aubert <raubert@student.'.fr>
2021-02-15 11:03:24 +0100

    fixed footer

--- static/js/detail.js
+++ static/js/detail.js
@@ -9,7 +9,7 @@ const makeContent = () => {
     })
     if (project.title.length > 0) {
         container.innerHTML +=
-        `<div class="row">
+        `<div class="row d-flex justify-content-center">
             <h1 class="title">${project.title}</h1>
         </div>`;
     }
@@ -19,14 +19,22 @@ const makeContent = () => {
     }
     if (project.content.length > 0) {
         container.innerHTML +=
-        `<div class="row content">${project.content}</div>`;
+        `<div class="row content d-flex justify-content-center">
+            <div>${project.content}</div>
+        </div>`;
     }
     if (project.article_link.length > 0) {
         container.innerHTML +=
-        `<div class="row"><p class="content">You can check out the full project by <a href="${project.article_link}" target="blank" class="link">clicking here</a>.</p></div>`;
+        `<div class="row content d-flex justify-content-center">
+            <p>You can check out the full project by <a href="${project.article_link}" target="blank" class="link">clicking here</a>.</p>
+        </div>`;
     }
     container.innerHTML +=
-    `<a href="../../index.html"><button type="button" class="btn btn-dark my-btn">Back to the grid</button></a>`;
+    `
+    <div class="row">
+        <a href="../../index.html"><button type="button" class="btn btn-dark my-btn">Back to the grid</button></a>
+    </div>
+    `;
 }
 
 window.addEventListener('load', makeContent);
