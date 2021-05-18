65b43202ecd8e1b7d223cf264198a511566d14a9
Raphaëlle Aubert <raubert@student.'.fr>
2021-05-18 17:40:46 +0200

    better spacing

--- static/js/detail.js
+++ static/js/detail.js
@@ -28,20 +28,20 @@ const makeContent = () => {
     if (project.content.length > 0) {
         container.innerHTML +=
         `<div class="row content d-flex justify-content-center">
-            <div class="col-md-6 p-2">${project.content}</div>
+            <div class="col-11 col-md-6">${project.content}</div>
         </div>`;
     }
     if (project.article_link.length > 0) {
         container.innerHTML +=
         `<div class="row content d-flex justify-content-center">
-            <div class="col-md-6 p-2">
+            <div class="col-11 col-md-6">
                 <p>You can check out the full project by <a href="${project.article_link}" target="blank" class="link">clicking here</a>.</p>
             </div>
         </div>`;
     }
     container.innerHTML +=
     `<div class="row content d-flex justify-content-center">
-        <div class="col-md-6 p-2">
+        <div class="col-11 col-md-6">
             <a href="../../index.html"><button type="button" class="btn btn-dark my-btn">Back</button></a>
         </div>
     </div>`;
